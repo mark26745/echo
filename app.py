ea08b950d7890edd27320e4006f14c2826d92b6c
mark26745 <markngugo@gmail.com>
2022-10-03 13:22:22 +0300

    Added get method to host

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI,Body
 app = FastAPI()
 
+
+
+@app.get("/",status_code=200)
+async def base():
+    return None
+
+
 # Dummy route to test callbacks
 @app.post("/dump")
 async def dump(dump = Body()):
