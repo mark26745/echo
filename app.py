d5cdf6e262982bc769a45920d560965be29e49e6
mark26745 <markngugo@gmail.com>
2022-10-11 11:48:22 +0300

    Removed models

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-from fastapi import FastAPI,Body,HTTPException
+from fastapi import FastAPI,Body
 app = FastAPI()
 
 
 # Dummy route to test callbacks
-@app.post("/dump",
-response_model=SensorDataRead)
+@app.post("/dump")
 async def dump(dump = Body()):
     print(dump)
     return dump
