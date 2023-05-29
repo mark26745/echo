4b78c7c22bf2ed4d90a091ddad87561040e9d202
Nmugi <72761972+mark26745@users.noreply.github.com>
2023-05-29 12:33:31 +0300

    Made the server request method agnostic

---
+++ dev.sh
@@ -0,0 +1,1 @@
+cargo watch -E PORT=3000 -B 1 -x check -x test -x run 
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-use actix_web::{get, post,App, HttpResponse, HttpServer, Responder, middleware::Logger};
+use actix_web::web;
+use actix_web::{get, middleware::Logger, App, HttpResponse, HttpServer, Responder};
 
 #[get("/health")]
 async fn health() -> impl Responder {
     HttpResponse::Ok()
 }
 
-#[post("/dump")]
 async fn dump(req_body: String) -> impl Responder {
     HttpResponse::Ok().body(req_body)
 }
@@ -15,20 +15,17 @@ async fn main() -> std::io::Result<()> {
     std::env::set_var("RUST_LOG", "actix_web=info");
     env_logger::init();
     let port = match std::env::var("PORT") {
-        Ok(val) => val.parse::<u16>().unwrap()    ,
+        Ok(val) => val.parse::<u16>().unwrap(),
         Err(_e) => 3000,
     };
-    HttpServer::new(move || {  
+    HttpServer::new(move || {
         let logger = Logger::default();
         App::new()
-        .wrap(logger)
+            .wrap(logger)
             .service(health)
-            .service(dump)
+            .service(web::resource("/").route(web::to(dump)))
     })
     .bind(("0.0.0.0", port))?
     .run()
     .await
-
-
-
 }
